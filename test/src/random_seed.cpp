c86858c858ecb07917c85b6707650644e24b3e68
kena <kena.e8ac99@gmail.com>
2021-06-19 10:23:20 +0900

    test: Improve a test

--- test/src/random_seed.cpp
+++ test/src/random_seed.cpp
@@ -1,12 +1,29 @@
 #include "random/random_seed.hpp"
 
+#include <array>
 #include <boost/ut.hpp>
 
 int main()
 {
-  auto seed1 = reki::random_seed{}();
+  using namespace boost::ut;
 
-  auto seed2 = reki::random_seed{}();
+  "Different values if generated on different positon"_test =
+    []()
+    {
+      constexpr auto seed1 = reki::random_seed{}();
 
-  boost::ut::expect(boost::ut::neq(seed1, seed2));
+      constexpr auto seed2 = reki::random_seed{}();
+
+      expect(neq(seed1, seed2));
+    };
+
+  "Same values if generated on same position"_test =
+    []()
+    {
+      std::array<reki::random_seed::result_type, 2> seeds{};
+
+      std::ranges::generate(seeds, reki::random_seed{});
+
+      expect(eq(seeds[0], seeds[1]));
+    };
 }
